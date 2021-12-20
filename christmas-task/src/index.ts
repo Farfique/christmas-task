3f58ef24e8b8984d30fbebdd3f31c9d1f2b4f72b
Anna Mikhailova <petunova@gmail.com>
2021-12-20 09:39:12 +0300

    docs: show report to the examiner

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,5 +1,15 @@
 import App from './ts/app';
 import './styles.scss';
+import { reportText } from './ts/utils/reportFile';
+
 
 const app = new App();
 app.start();
+writeReport();
+
+function writeReport(){
+  reportText.forEach((str) => {
+    console.log(str);
+  })
+}
+
